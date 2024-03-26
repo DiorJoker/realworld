e709c7d1c463b0c3fb2f46041c830497f0283762
poisonmachine <siz.osk.aiti.huku@gmail.com>
2024-03-26 23:53:26 +0900

    aws_quest

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-require "rack/cors"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +10,8 @@ module Realworld
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    require "rack/cors"
 
     # Configuration for the application, engines, and railties goes here.
     #
